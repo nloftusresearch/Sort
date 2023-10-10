78bd54e7a9ca5ef75f26304a04a548077c0297c4
Taylor Markham <59581164+Taylordmark@users.noreply.github.com>
2023-10-10 12:21:30 -0400

    Saving for meeting 1

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import numpy as np
+from scipy.stats import beta
 
-object_type_distributions = []
-for obj_class in range(5):
-    object_type_distributions.append(np.empty((1, 5)))
+# Your list of numbers
+data = [.1,.2,.4]
+new_number = .7
+# Fit a beta distribution to the data
+alpha, beta_params, loc, scale = beta.fit(data, floc=0, fscale=1)
 
-for o in object_type_distributions:
-    print(o)
-    print()
+# Parameters of the fitted beta distribution
+print(f"Fitted Parameters: alpha = {alpha}, beta = {beta_params}, loc = {loc}, scale = {scale}")
+
+# Calculate the PDF for the new number
+pdf = beta.pdf(new_number, alpha, beta_params, loc, scale)
+print(f"PDF for {new_number} = {pdf}")
